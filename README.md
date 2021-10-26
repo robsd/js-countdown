a44f7cb4e7d6d65ac0180ec12732b8ad1c8796dd
Robert Dixon <79385125+robsd@users.noreply.github.com>
2021-10-26 22:15:06 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# js-countdown ⏲
+
+A little countdown widget that counts down to a specific date in days, hours, minutes and seconds.
+
+## Demo
+
+https://robsd.github.io/js-countdown
---
+++ js/script.js
@@ -0,0 +1,19 @@
+target = '25/12/2021'; // Countdown To (DD/MM/YYYY)
+
+days = document.getElementById('days');
+hours = document.getElementById('hours');
+minutes = document.getElementById('minutes');
+seconds = document.getElementById('seconds');
+
+setInterval(function() {
+	now = new Date().getTime();
+	targetSplit = target.split('/');
+	targetParsed = new Date(targetSplit[2], targetSplit[1] - 1, targetSplit[0]);
+	difference = targetParsed - now;
+	if (difference > 0) {
+		days.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24));
+		hours.innerHTML = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		minutes.innerHTML = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+		seconds.innerHTML = Math.floor((difference % (1000 * 60)) / 1000);
+	}
+}, 1000);
